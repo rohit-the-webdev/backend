31e3c6d47c245158d684ef97ed82f0d353a0b7e8
Hytech842 <rohitkale842@gmail.com>
2026-01-09 13:12:14 +0530

    minor changes in db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) return;
+
   try {
     await mongoose.connect(process.env.MONGO_URI);
-
+    isConnected = true;
     console.log("MongoDB connected");
   } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
+    console.error("MongoDB connection error:", error.message);
+    throw error; // let request fail gracefully
   }
 };
 

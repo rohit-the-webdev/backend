ce55cb55ee9d0747ebd4dab099626be61d7b0e77
Hytech842 <rohitkale842@gmail.com>
2026-01-09 13:32:30 +0530

    Test 3

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 let isConnected = false;
 
-export default async function connectDB() {
+const connectDB = async () => {
   if (isConnected) return;
 
   await mongoose.connect(process.env.MONGO_URI);
   isConnected = true;
-}
+  console.log("MongoDB connected");
+};
+
+module.exports = connectDB;
